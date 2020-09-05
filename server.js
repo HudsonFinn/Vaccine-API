7415e0ca511cfe964cdfcfd197fd2f40c6fb182d
Jang Belche <jangbel99@gmail.com>
2020-09-05 14:16:58 +0200

    set up express file

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () =>
+  console.log(`API running on port: ${port}`),
+);
